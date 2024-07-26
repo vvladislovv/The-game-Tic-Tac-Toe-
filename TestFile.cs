3f55f507a4def95a6562a2f39efcfa56944a03cc
vvladislovv <vlad.yelcheninov@gmail.com>
2024-07-26 13:53:29 +0300

    Добавьте файлы проекта.

---
+++ TestFile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Ling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ing[] bag = new int[];
+            bag[0] = 12;
+            int[] tempBag new int(bag.Lenght + 1);
+
+            for (int i = 0; i < bag.Lenght; i++)
+            {
+                tempBag[i] = bag[i];
+                
+            }
+        }
+    }
+}
